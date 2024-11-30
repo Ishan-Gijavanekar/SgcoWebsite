65bc541a412ae6725b376f6163c09b457c5a8e69
Ishan-Gijavanekar <isg.sachin@gmail.com>
2024-11-30 19:04:48 +0530

    Added basename in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   return (
     <>
       <Navbar />
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element = {<Home />}/>
           <Route path="/about" element = {<About />}/>
